9609056124a194eba2b5b49c3bf9aa90ab0b9ad8
leej3 <johnleenimh+kraken@gmail.com>
2019-01-22 14:40:01 -0500

    don't break up the x11 libraries

--- cmake/Findm4.cmake
+++
@@ -1,6 +0,0 @@
-find_program(M4_BIN m4)
-
-set(M4_BIN "${M4_BIN}" PARENT_SCOPE)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(M4 DEFAULT_MSG M4_BIN)
--- cmake/Finduuid.cmake
+++
@@ -1,9 +0,0 @@
-find_package(PkgConfig)
-pkg_check_modules(PC QUIET uuid)
-set(UUID_DEFINITIONS ${PC_CFLAGS_OTHER})
-
-find_path(UUID_INCLUDE_DIRS uuid/uuid.h HINTS ${PC_INCLUDE_DIRS})
-find_library(UUID_LIBRARIES NAMES uuid HINTS ${PC_LIBRARY_DIRS})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(uuid DEFAULT_MSG UUID_LIBRARIES UUID_INCLUDE_DIRS)
