68afcfeaea8fe3d9796c87212c41b4c4ef0b5d84
leej3 <johnleenimh@gmail.com>
2020-07-02 17:05:00 -0400

    do not set osx sysroot

--- conda_build/build.sh
+++ conda_build/build.sh
@@ -2,7 +2,13 @@
 # See https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html#an-aside-on-cmake-and-sysroots
 declare -a CMAKE_PLATFORM_FLAGS
 if [[ ${HOST} =~ .*darwin.* ]]; then
-  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+    # specify the compiler
+    export CMAKE_C_COMPILER=${CC}
+
+# where is the target environment
+ export CMAKE_FIND_ROOT_PATH="${PREFIX}:${BUILD_PREFIX}/${HOST}/sysroot"
+
+#  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
   export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,-dead_strip_dylibs//g")
 else
   CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
@@ -15,6 +21,8 @@ cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
 -DCMAKE_BUILD_TYPE=Release \
 -DBUILD_SHARED_LIBS=ON \
 -DNO_SONAME=ON \
+-DREMOVE_BUILD_PARITY_CHECKS=ON \
+-DUSE_OMP=OFF \
 -GNinja \
 $SRC_DIR \
 ${CMAKE_PLATFORM_FLAGS[@]} 
